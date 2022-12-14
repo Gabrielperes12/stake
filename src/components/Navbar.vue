dc0dc753020002215b7d95ef540bae20e1356b17
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-12-14 00:35:54 -0300

    aula21

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div id="nav">
+        <router-link id="logo-url" to="/">
+          <img id="logo" :src="logo" :alt="alt">
+        </router-link>
+        <router-link to="/">Fazer Pedido</router-link>
+        <router-link to="/pedidos">Pedidos</router-link>
+      </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style scoped>
+
+</style>
