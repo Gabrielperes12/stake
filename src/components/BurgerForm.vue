58e8374a00471d010b3616437047d08f0aea3ebe
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-12-16 02:57:50 -0300

    pão e nome do cliente

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+       <p>Menssagem</p> 
+       <div>
+        <form id="burger-form">
+            <div id="input-container">
+                <label for="nome">Nome do cliente:</label>
+                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu nome">
+            </div>
+
+            <div id="input-container">
+                <label for="pao">Escolha o pão:</label>
+                <select name="pao" id="pao" v-model="pao"></select>
+                <option value="">Selecione o seu pão</option>
+                <option value="">Integral</option>
+                
+            </div>
+
+        </form>
+       </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'BurgerForm'
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <Banner/>
   <div class="main-container">
     <h1>Monte seu Hamburguer:</h1>
-    <p>Formulario</p>
+    <BurgerForm/>
   </div>
 </template>
 
 <script>
 import Banner from '../components/Banner.vue';
+import BurgerForm from '../components/BurgerForm.vue';
   export default{
     name: 'Home',
     components:{
-      Banner
+      Banner,
+      BurgerForm
     }
   }
 </script>
